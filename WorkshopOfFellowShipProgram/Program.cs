d656ca60a2bdc42cb3cdc249259dd8f4c579a64f
vinaykumar7081 <vinaykumarvinay165@gmail.com>
2022-05-18 09:35:08 +0530

    [VINAY] Add: Calculating the power of tow Number But Number must be Less than 32 and Greater than 0

---
+++ WorkshopOfFellowShipProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using WorkshopOfFellowShipProgram;
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        TablePowerOfTwo result = new TablePowerOfTwo();
+        Console.WriteLine("Enter a number to To display the Power of Two");
+        int num = Convert.ToInt32(Console.ReadLine());
+        result.Table(num);
+    }
+}
---
+++ WorkshopOfFellowShipProgram/TablePowerOfTwo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkshopOfFellowShipProgram
+{
+    public class TablePowerOfTwo
+    {
+        public void Table(int num)
+        {
+            if (0 < num && num <= 31)
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    Console.WriteLine(2 + "^" + i + "=" + Math.Pow(2, i));
+                }
+            }
+            else
+            {
+                Console.WriteLine("OverStackFlow");
+            }
+        }
+    }
+}
